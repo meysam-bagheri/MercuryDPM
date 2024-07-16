c258ffd080c7a6c4fe62858309abbb977a7ac8bc
Tóth János <gomba007@gmail.com>
2024-07-16 10:55:30 +0200

    Tidy helpers.
    Do some const refs.
    
    Add `stringFornat()`, an `snprinf()` like helper for strings.

--- Kernel/Helpers/StringHelpers.h
+++ Kernel/Helpers/StringHelpers.h
@@ -29,6 +29,7 @@
 #include "Math/ExtendedMath.h"
 
 #include <string>
+#include <memory>
 
 namespace helpers
 {
@@ -70,6 +71,19 @@ namespace helpers
             stm << val << ' ';
         }
         return stm.str();
+    }
+
+    template <typename... Args>
+    std::string stringFormat(const std::string& format, Args... args)
+    {
+        int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) + 1; // Extra space for '\0'
+        if (size_s <= 0) {
+            logger(ERROR, "snprintf() failed");
+        }
+        auto size = static_cast<size_t>(size_s);
+        std::unique_ptr<char[]> buf(new char[size]);
+        std::snprintf(buf.get(), size, format.c_str(), args...);
+        return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
     }
 }
 
